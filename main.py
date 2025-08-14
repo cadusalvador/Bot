2dbe8a64f8c12ef4cc0ae25dbd49eb42b01e1018
luis-tiberio <luisotaviotiberio@gmail.com>
2025-08-14 04:52:50 -0300

    refatoração 2

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+import hashlib
+import json
+from typing import    Dict, Any
 from flask import Flask, request,jsonify
 import requests
 import os
 
+WEBHOOK_URL = os.getenv("SEATALK_WEBHOOK_URL")
+SIGNING_SECRET = os.getenv("SEATALK_SIGN_SECRET")
+EVENT_VERIFICATION = os.getenv("SEATALK_EVENT_VERIFICATION")
+
 app = Flask(__name__)
 
-WEBHOOK_URL = os.getenv("SEATALK_WEBHOOK_URL")
+
+def is_valid_signature(signing_secret: bytes, body: bytes, signature: str) -> bool:
+    # ref: https://open.seatalk.io/docs/server-apis-event-callback
+    return hashlib.sha256(body + signing_secret).hexdigest() == signature
+
+
+@app.route("/bot-callback", methods=["POST"])
+def bot_callback_handler():
+    body: bytes = request.get_data()
+    signature: str = request.headers.get("signature")
+    # 1. validate the signature
+    if not is_valid_signature(SIGNING_SECRET, body, signature):
+        return ""
+    # 2. handle events
+    data: Dict[str, Any] = json.loads(body)
+    event_type: str = data.get("event_type", "")
+    if event_type == EVENT_VERIFICATION:
+        return data.get("event")
 
 @app.route("/", methods=["POST"])
 def webhook():
