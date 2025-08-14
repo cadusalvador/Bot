e40686c0e4ff868c43d57d125d42a85ef6909581
luis-tiberio <luisotaviotiberio@gmail.com>
2025-08-14 04:19:27 -0300

    REFATORAÇÃO

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-import cs_bot
-from cs_bot import StartupConfig
-from cs_bot.adapters import sop_bot
+from crypt import methods
+from flask import Flask, request,jsonify
+import requests
+import os
 
+app = Flask(__name__)
 
-config = {
-    "adapter": {
-        "app_id": "ODI1ODIyODY0NDYy",
-        "app_secret": "5srPiZuFhH84GkUl2qqjRFBDuFnlQZQ4",
-        "signing_secret": "4MPu_ZvbEOUIaAohr4leF09VwG2sKutB"
-    }
-}
+WEBHOOK_URL = os.getenv("SEATALK_WEBHOOK_URL")
 
-cs_bot.init(StartupConfig.parse_obj(config))
-cs_bot.register_adapter(sop_bot.Adapter)
-cs_bot.load_plugin("plugin.hello")
+@app.route("/", methods=["POST"])
+def webhook():
+    data = request.json
 
+    print("Dados recebidos:", data)
+
+    if WEBHOOK_URL:
+        payload = {"content": "WAAAALLL-E"}
+        resp = requests.post(WEBHOOK_URL, json=payload)
+
+        if resp.status_code == 200:
+            print("Mensagem enviada com sucesso!")
+        else:
+            print(f"Falha ao enviar: {resp.status_code} - {resp.text}")
+
+    return jsonify({"status": "OK"}), 200
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot Seatalk ativo!", 200
 
 if __name__ == '__main__':
-    cs_bot.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
